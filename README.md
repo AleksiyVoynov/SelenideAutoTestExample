c5ade0140dfc34079bafcccf862319d6b983ff45
AleksiyVoynov <leshavoynov@gmail.com>
2024-06-28 12:14:02 +0300

    com1

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-# Requirements:
-* MacOS Sonoma and above
-* Java 17
-* Gradle
-* Chrome
-* Safari
-* FireFox
-
-
 # Guide:
 
 ## To run tests and generate Allure report:
-
+* open terminal
+* cd (path to /SeleniumWebDriverAutotest)
 * run `gradle clean`
 * run `gradle test`
 
 ## To see a report:
-
 1. run `allure serve build/allure-results `
--- src/test/java/app/google/GoogleSearchTest.java
+++ src/test/java/app/google/GoogleSearchTest.java
@@ -1,31 +1,68 @@
 package app.google;
 
-import app.BaseTest;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.GooglePage;
 
 import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.open;
 
-public class GoogleSearchTest extends BaseTest {
+public class GoogleSearchTest {
 
-    @Test(priority = 1, description = "test google search")
+    private final String testData = "facebook";
+    GooglePage googlePage;
+    @BeforeClass
+    @Step("setting up web driver")
+    @Parameters({"browser"})
+    public void setUp(@Optional("default") String browser) throws MalformedURLException {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.headless = false;
+        Configuration.timeout = 3000;
+        if(browser.equals("default")) {
+            open();
+        } else {
+            Configuration.browser = "chrome";
+        }
+         googlePage = new GooglePage().openPage();
+    }
+
+    @Test(priority = 1, description = "test google search 1")
     @Description("""
             Test Description:
             1. go to google page
             2. make search
             3. validate result""")
-    public void userCanSearch() throws MalformedURLException {
-        var testData = "facebook";
-        new GooglePage()
-                .openPage()
-                .searchFor(testData)
+    public void userCanSearch1() {
+        googlePage.searchFor(testData)
                 .results()
                 .shouldHave(sizeGreaterThan(10))
                 .get(0)
-                .shouldHave(text(testData));
+                .shouldHave(text(testData))
+                .shouldBe(visible);
+    }
+
+    @Test(priority = 2, description = "test google search 2")
+    @Description("""
+            Test Description:
+            1. go to google page
+            2. make search
+            3. validate result""")
+    public void userCanSearch2() {
+        googlePage.searchFor(testData)
+                .results()
+                .get(10)
+                .scrollTo()
+                .shouldHave(or("first or second value", text(testData), text("google")))
+                .shouldBe(visible);
     }
 }
